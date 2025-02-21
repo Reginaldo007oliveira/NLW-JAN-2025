6ed563204527291177d665fa5d30bf01b6bd56f9
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2025-02-21 16:35:57 -0300

    Exemplo de botão

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
+
 import { Button } from "./components/button";
+import { IconButton } from "./components/icon-button";
+
 import "./globals.css";
-import { ArrowRight, Mail, Radio, User } from 'lucide-react' // npm install lucide-react
+import { ArrowRight, Mail, Radio, User, Copy } from 'lucide-react' // npm install lucide-react
 
 
 export default function Home() {
   return (
     <main>
-      
+      <Button>
+         Enviar
+        <ArrowRight/>
+      </Button>
+
+      <IconButton>
+        
+             </IconButton>
     </main>
   );
 }
